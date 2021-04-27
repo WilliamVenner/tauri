41f7dac70508454306555d7a6f8c5c9d723b5eb6
William Venner <william@venner.io>
2021-04-28 00:03:34 +0100

    Allow .dylib frameworks

--- tooling/bundler/src/bundle/macos_bundle.rs
+++ tooling/bundler/src/bundle/macos_bundle.rs
@@ -737,9 +737,16 @@ fn copy_frameworks_to_bundle(bundle_directory: &Path, settings: &Settings) -> cr
         .expect("Couldn't get framework filename");
       common::copy_dir(&src_path, &dest_dir.join(&src_name))?;
       continue;
+    } else if framework.ends_with(".dylib") {
+      let src_path = PathBuf::from(framework);
+      let src_name = src_path
+        .file_name()
+        .expect("Couldn't get dylib filename");
+      common::copy_file(&src_path, &dest_dir.join(&src_name))?;
+      continue;
     } else if framework.contains('/') {
       return Err(crate::Error::GenericError(format!(
-        "Framework path should have .framework extension: {}",
+        "Framework path should have .framework or .dylib extension: {}",
         framework
       )));
     }
